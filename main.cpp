fad9f243dbc2b555fc04957118de6a258b5136f1
HannuKarjalainen <143908228+HannuKarjalainen@users.noreply.github.com>
2023-09-26 21:48:37 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,54 @@
+#include "asunto.h"
+#include "katutaso.h"
+#include "kerrostalo.h"
+
+int main()
+{
+
+    Kerrostalo kerrostalo;
+    double hinta = 1;
+    kerrostalo.laskeKulutus(hinta);
+/*
+    delete kerrostalo.eka;
+    delete kerrostalo.toka;
+    delete kerrostalo.kolmas;
+*/
+    return 0;
+}
+
+/*
+Asunto luotu
+Asunto luotu
+Asunto luotu
+Asunto luotu
+Kerros luotu
+Asunto luotu
+Asunto luotu
+Katutaso luotu
+Asunto luotu
+Asunto luotu
+Asunto luotu
+Asunto luotu
+Kerros luotu
+Asunto luotu
+Asunto luotu
+Asunto luotu
+Asunto luotu
+Kerros luotu
+Kerrostalo luotu
+Maaritellaan koko kerrostalon kaikki asunnot
+Maaritetaan 2 kpl katutason asuntoja
+Asunto maaritetty asukkaita = 2 nelioita = 100
+Asunto maaritetty asukkaita = 2 nelioita = 100
+Maaritetaan 4 kpl kerroksen asuntoja
+Asunto maaritetty asukkaita = 2 nelioita = 100
+Asunto maaritetty asukkaita = 2 nelioita = 100
+Asunto maaritetty asukkaita = 2 nelioita = 100
+Asunto maaritetty asukkaita = 2 nelioita = 100
+Maaritetaan 4 kpl kerroksen asuntoja
+Asunto maaritetty asukkaita = 2 nelioita = 100
+Asunto maaritetty asukkaita = 2 nelioita = 100
+Asunto maaritetty asukkaita = 2 nelioita = 100
+Asunto maaritetty asukkaita = 2 nelioita = 100
+Kerrostalon kulutus = 2800
+ */
